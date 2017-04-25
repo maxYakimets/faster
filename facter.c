af9230f112415272a983176f7104e1b71bc8869d
maxYakimets <maxyakimets@gmail.com>
2017-04-25 19:29:28 +0300

    created faster.c

---
+++ facter.c
@@ -0,0 +1,12 @@
+//created to be fast
+static double evalSpeedDifference( double boatSpeed, double windSpeed, double windAngle );
+static double evalTrueWindSpeed( double windSpeed, double windAngle );
+static double[][] initPolarDiagram( void );
+
+void main( int )
+{
+//init the bus
+//listen to the bus
+//parce the message
+//evaluate the result
+}
